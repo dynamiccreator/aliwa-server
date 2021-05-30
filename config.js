9c8396f94b8f97afae765d658054c098c82741a4
dev <dev@dev-VirtualBox>
2021-05-30 22:29:24 +0200

    aliwa server -prepared for testnet

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = function () {
      cnf_read_block_height=1700000; //(0 for sync from block 0 or a higher value for quick testing)
     
     //Maria DB
-     cnf_db_user="alias_db_user";
+     cnf_db_user="";
      cnf_db_password="";
      cnf_db_database="";
      
