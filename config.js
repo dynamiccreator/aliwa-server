80d318ed1d9cddbec693814ea10cc3ea90778e7f
HLXEasy <hlxeasy@gmail.com>
2021-06-16 18:53:01 +0200

    Implemented configuration option for wallet and database host. Not finished yet!

--- config.js
+++ config.js
@@ -1,18 +1,20 @@
 module.exports = function () {
     //alias.cnf 
-     cnf_username = "";
-     cnf_password = "";
-     cnf_read_block_height=1700000; //(0 for sync from block 0 or a higher value for quick testing)
-    
+    cnf_host = "localhost";
+    cnf_username = "";
+    cnf_password = "";
+    cnf_read_block_height = 1700000; //(0 for sync from block 0 or a higher value for quick testing)
+
     //Maria DB
-     cnf_db_user="";
-     cnf_db_password="";
-     cnf_db_database="";
-     
-     //load prices from coingecko
-     cnf_get_alias_prices=true;
-     //define a donation address shown in user's wallet
-     cnf_donation_address="server donation address";
+    cnf_db_host = "localhost";
+    cnf_db_user = "";
+    cnf_db_password = "";
+    cnf_db_database = "";
+
+    //load prices from coingecko
+    cnf_get_alias_prices = true;
+    //define a donation address shown in user's wallet
+    cnf_donation_address = "server donation address";
 };
 
 
